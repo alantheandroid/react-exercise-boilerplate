9f9f5c2f607a9523e3eebacd5e42bb9838705e8c
Alessio Ilardi <alessio.ilardi@gmail.com>
2022-08-01 18:46:10 +0200

    feat: add filtering logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import ContactList from "./component/contactList";
-import SearchField from "./component/searchField";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <SearchField />
       <ContactList />
     </div>
   );
--- src/component/contactList.js
+++ src/component/contactList.js
@@ -1,10 +1,11 @@
 import data from "../data/db";
 import React from "react";
+import SearchField from "./searchField";
 
 class NewRow extends React.Component {
   render() {
     return (
-      <tr>
+      <tr className="newRow">
         <td>{this.props.firstName}</td>
         <td>{this.props.lastName}</td>
         <td>{this.props.phone}</td>
@@ -17,20 +18,53 @@ class NewRow extends React.Component {
 }
 
 class ContactList extends React.Component {
-  state = { data };
+  state = {
+    contacts: data,
+    active: data,
+  };
+
+  filterList = (text) => {
+    const filteredList = this.state.contacts.filter(
+      (contact) =>
+        text.toLowerCase() === "" ||
+        contact.first_name.toLowerCase().includes(text) ||
+        contact.first_name.includes(text) ||
+        contact.last_name.toLowerCase().includes(text) ||
+        contact.last_name.includes(text) ||
+        contact.phone.includes(text)
+    );
+    console.log(text, filteredList);
+    this.setState({
+      active: filteredList,
+    });
+  };
 
   render() {
     return (
-      <table>
-        {this.state.data.map((contact) => (
-          <NewRow
-            firstName={contact.first_name}
-            lastName={contact.last_name}
-            phone={contact.phone}
-            image={contact.image}
-          />
-        ))}
-      </table>
+      <div className="container">
+        <SearchField filter={this.filterList} />
+        <table className="contactList">
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Phone Number</th>
+              <th>Contact Picture</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.active.map((contact) => (
+              <NewRow
+                key={contact.first_name + contact.last_name}
+                firstName={contact.first_name}
+                lastName={contact.last_name}
+                phone={contact.phone}
+                image={contact.image}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
