d06b7581c1f3ec00b249c18ef023fde80f16e79e
Alessio Ilardi <alessio.ilardi@gmail.com>
2022-08-01 12:20:33 +0200

    feat: add variable to array before export

--- src/data/db.js
+++ src/data/db.js
@@ -1,62 +1,74 @@
-export default [
+const data = [
   {
     first_name: "Maria",
     last_name: "Rossi",
     phone: "32909878902",
-    image: "https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light"
+    image:
+      "https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light",
   },
   {
     first_name: "Paolo",
     last_name: "Rossini",
     phone: "329068968902",
-    image: "https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortCurly&accessoriesType=Blank&hairColor=Brown&facialHairType=MoustacheFancy&facialHairColor=Blonde&clotheType=ShirtVNeck&clotheColor=Gray02&eyeType=WinkWacky&eyebrowType=UnibrowNatural&mouthType=Default&skinColor=Light"
+    image:
+      "https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortCurly&accessoriesType=Blank&hairColor=Brown&facialHairType=MoustacheFancy&facialHairColor=Blonde&clotheType=ShirtVNeck&clotheColor=Gray02&eyeType=WinkWacky&eyebrowType=UnibrowNatural&mouthType=Default&skinColor=Light",
   },
   {
     first_name: "Giovanni",
     last_name: "Bianchi",
     phone: "398790508283",
-    image: "https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light"
+    image:
+      "https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light",
   },
   {
     first_name: "Valerio",
     last_name: "Verdi",
     phone: "345987899238",
-    image: "https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortFlat&accessoriesType=Kurt&hairColor=Platinum&facialHairType=BeardMedium&facialHairColor=Black&clotheType=Hoodie&clotheColor=Gray01&eyeType=Happy&eyebrowType=RaisedExcitedNatural&mouthType=Sad&skinColor=Tanned"
+    image:
+      "https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortFlat&accessoriesType=Kurt&hairColor=Platinum&facialHairType=BeardMedium&facialHairColor=Black&clotheType=Hoodie&clotheColor=Gray01&eyeType=Happy&eyebrowType=RaisedExcitedNatural&mouthType=Sad&skinColor=Tanned",
   },
   {
     first_name: "Marco",
     last_name: "Arona",
     phone: "098767837884",
-    image: "https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortFlat&accessoriesType=Kurt&hairColor=Red&facialHairType=Blank&facialHairColor=Brown&clotheType=Hoodie&clotheColor=Gray01&eyeType=Side&eyebrowType=RaisedExcitedNatural&mouthType=ScreamOpen&skinColor=Pale"
+    image:
+      "https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortFlat&accessoriesType=Kurt&hairColor=Red&facialHairType=Blank&facialHairColor=Brown&clotheType=Hoodie&clotheColor=Gray01&eyeType=Side&eyebrowType=RaisedExcitedNatural&mouthType=ScreamOpen&skinColor=Pale",
   },
   {
     first_name: "Elena",
     last_name: "Rossi",
     phone: "45234424523",
-    image: "https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraightStrand&accessoriesType=Kurt&hairColor=SilverGray&facialHairType=MoustacheFancy&facialHairColor=Brown&clotheType=BlazerSweater&clotheColor=Gray02&eyeType=Happy&eyebrowType=SadConcernedNatural&mouthType=Concerned&skinColor=Black"
+    image:
+      "https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraightStrand&accessoriesType=Kurt&hairColor=SilverGray&facialHairType=MoustacheFancy&facialHairColor=Brown&clotheType=BlazerSweater&clotheColor=Gray02&eyeType=Happy&eyebrowType=SadConcernedNatural&mouthType=Concerned&skinColor=Black",
   },
   {
     first_name: "Giovanna",
     last_name: "Verdi",
     phone: "12564534423",
-    image: "https://avataaars.io/?avatarStyle=Circle&topType=LongHairFroBand&accessoriesType=Wayfarers&hairColor=Red&facialHairType=Blank&facialHairColor=Blonde&clotheType=ShirtScoopNeck&clotheColor=PastelOrange&eyeType=Side&eyebrowType=UnibrowNatural&mouthType=Twinkle&skinColor=Tanned"
+    image:
+      "https://avataaars.io/?avatarStyle=Circle&topType=LongHairFroBand&accessoriesType=Wayfarers&hairColor=Red&facialHairType=Blank&facialHairColor=Blonde&clotheType=ShirtScoopNeck&clotheColor=PastelOrange&eyeType=Side&eyebrowType=UnibrowNatural&mouthType=Twinkle&skinColor=Tanned",
   },
   {
     first_name: "Ernesto",
     last_name: "Cardillo",
     phone: "02345878293",
-    image: "https://avataaars.io/?avatarStyle=Circle&topType=ShortHairTheCaesar&accessoriesType=Round&hairColor=Black&facialHairType=BeardLight&facialHairColor=Red&clotheType=GraphicShirt&clotheColor=PastelGreen&graphicType=SkullOutline&eyeType=Cry&eyebrowType=SadConcerned&mouthType=Serious&skinColor=DarkBrown"
+    image:
+      "https://avataaars.io/?avatarStyle=Circle&topType=ShortHairTheCaesar&accessoriesType=Round&hairColor=Black&facialHairType=BeardLight&facialHairColor=Red&clotheType=GraphicShirt&clotheColor=PastelGreen&graphicType=SkullOutline&eyeType=Cry&eyebrowType=SadConcerned&mouthType=Serious&skinColor=DarkBrown",
   },
   {
     first_name: "Mario",
     last_name: "Mannino",
     phone: "078986747838",
-    image: "https://avataaars.io/?avatarStyle=Circle&topType=NoHair&accessoriesType=Round&facialHairType=Blank&facialHairColor=Blonde&clotheType=ShirtCrewNeck&clotheColor=PastelYellow&eyeType=Wink&eyebrowType=UnibrowNatural&mouthType=Smile&skinColor=Tanned"
+    image:
+      "https://avataaars.io/?avatarStyle=Circle&topType=NoHair&accessoriesType=Round&facialHairType=Blank&facialHairColor=Blonde&clotheType=ShirtCrewNeck&clotheColor=PastelYellow&eyeType=Wink&eyebrowType=UnibrowNatural&mouthType=Smile&skinColor=Tanned",
   },
   {
     first_name: "Francesco",
     last_name: "Scala",
     phone: "093678283784",
-    image: "https://avataaars.io/?avatarStyle=Circle&topType=Eyepatch&accessoriesType=Wayfarers&hatColor=PastelOrange&facialHairType=Blank&facialHairColor=Blonde&clotheType=BlazerShirt&clotheColor=PastelGreen&eyeType=Side&eyebrowType=DefaultNatural&mouthType=Eating&skinColor=Tanned"
+    image:
+      "https://avataaars.io/?avatarStyle=Circle&topType=Eyepatch&accessoriesType=Wayfarers&hatColor=PastelOrange&facialHairType=Blank&facialHairColor=Blonde&clotheType=BlazerShirt&clotheColor=PastelGreen&eyeType=Side&eyebrowType=DefaultNatural&mouthType=Eating&skinColor=Tanned",
   },
-]
+];
+
+export default data;
