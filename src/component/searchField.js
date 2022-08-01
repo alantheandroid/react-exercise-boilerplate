854f535dfc5ceac63673f27d5b4d578ea5f63eed
Alessio Ilardi <alessio.ilardi@gmail.com>
2022-08-01 12:25:05 +0200

    feat: add "searchField" to handle list search and filter

---
+++ src/component/searchField.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class SearchField extends React.Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <label>
+          <input type={"search"}></input>
+        </label>
+      </div>
+    );
+  }
+}
+
+export default SearchField;
