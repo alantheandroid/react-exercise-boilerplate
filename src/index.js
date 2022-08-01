42c94243407a3523a661574f27293b8a3c7aa30a
Alessio Ilardi <alessio.ilardi@gmail.com>
2022-08-01 18:44:41 +0200

    feat: add "StrictMode"

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.Fragment>
-    <App />
-  </React.Fragment>
+  <StrictMode>
+    <React.Fragment>
+      <App />
+    </React.Fragment>
+  </StrictMode>
 );
